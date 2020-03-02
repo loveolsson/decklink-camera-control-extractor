538dc90481870b1e6e879bd4ede02773b1c23b0c
lolsson <lolsson@lolssonsMBP1898.bme>
2020-03-02 18:41:37 +0100

    xD

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include "include/DeckLinkAPI.h"
 
+#include <chrono>
+#include <thread>
+#include <signal.h>
+
+static volatile int keepRunning = 1;
+
+void intHandler(int dummy) {
+    keepRunning = 0;
+}
+
 int main() {
+	while (keepRunning) { 
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+	
 	return 0;
 }
