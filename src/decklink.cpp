18e99eb05ef1ef16ee573ca2ec76b924be58b853
lolsson <lolsson@lolssonsMBP1898.bme>
2020-03-02 23:30:20 +0100

    xD

--- src/decklink.cpp
+++ src/decklink.cpp
@@ -10,29 +10,32 @@
 static void unpack_v210(uint16_t *dst, const uint8_t *src, int width)
 {
     int i;
-    for (i = 0; i < width * 2 / 3; i++) {
-        *dst++ =  src[0]       + ((src[1] & 3)  << 8);
+    for (i = 0; i < width * 2 / 3; i++)
+    {
+        *dst++ = src[0] + ((src[1] & 3) << 8);
         *dst++ = (src[1] >> 2) + ((src[2] & 15) << 6);
         *dst++ = (src[2] >> 4) + ((src[3] & 63) << 4);
         src += 4;
     }
 }
 
-std::string ToHex(const uint8_t* buffer, size_t size){
-	std::stringstream str;
-	str.setf(std::ios_base::hex, std::ios::basefield);
-	//str.unsetf(std::ios::showbase);
-	//str.setf(std::ios::showbase);
-	str.setf(std::ios_base::uppercase);
-	str.fill('0');
-	//str.width(2);
-	
-	for(size_t i=0; i<size; ++i){
-		//str << std::hex << std::setw(2) << (unsigned short)(unsigned char)buffer[i];
-		//str << (unsigned short)(unsigned char)buffer[i];
-		str << std::setw(2) << (unsigned short)(uint8_t)buffer[i];
-	}
-	return str.str();
+std::string ToHex(const uint8_t *buffer, size_t size)
+{
+    std::stringstream str;
+    str.setf(std::ios_base::hex, std::ios::basefield);
+    //str.unsetf(std::ios::showbase);
+    //str.setf(std::ios::showbase);
+    str.setf(std::ios_base::uppercase);
+    str.fill('0');
+    //str.width(2);
+
+    for (size_t i = 0; i < size; ++i)
+    {
+        //str << std::hex << std::setw(2) << (unsigned short)(unsigned char)buffer[i];
+        //str << (unsigned short)(unsigned char)buffer[i];
+        str << std::setw(2) << (unsigned short)(uint8_t)buffer[i];
+    }
+    return str.str();
 }
 
 IDeckLink *GetFirstDeckLink()
@@ -60,28 +63,31 @@ DeckLinkReceiver::DeckLinkReceiver(IDeckLink *_deckLink)
     {
         std::cout << "DeckLink card has input" << std::endl;
 
-        if (this->deckLinkInput->QueryInterface(IID_IDeckLinkProfileAttributes, (void**)&attr) != S_OK) {
+        if (this->deckLinkInput->QueryInterface(IID_IDeckLinkProfileAttributes, (void **)&attr) != S_OK)
+        {
             std::cout << "Could not get IDeckLinkProfileAttributes." << std::endl;
             return;
         }
-        
+
         attr->GetFlag(BMDDeckLinkVANCRequires10BitYUVVideoFrames, &this->requires10bit);
 
-        if (this->requires10bit) {
+        if (this->requires10bit)
+        {
             std::cout << "Requires 10bit for VANC." << std::endl;
         }
 
         attr->Release();
 
-
-        if (this->deckLinkInput->SetCallback(this) != S_OK) {
+        if (this->deckLinkInput->SetCallback(this) != S_OK)
+        {
             std::cout << "Failed to set input callback." << std::endl;
             return;
         }
 
         BMDPixelFormat format = requires10bit ? bmdFormat10BitYUV : bmdFormat8BitYUV;
 
-        if (this->deckLinkInput->EnableVideoInput(bmdModeNTSC, format, bmdVideoInputEnableFormatDetection) != S_OK) {
+        if (this->deckLinkInput->EnableVideoInput(bmdModeNTSC, format, bmdVideoInputEnableFormatDetection) != S_OK)
+        {
             std::cout << "Failed to enable video stream." << std::endl;
             return;
         }
@@ -91,7 +97,8 @@ DeckLinkReceiver::DeckLinkReceiver(IDeckLink *_deckLink)
         //     return;
         // }
 
-        if (this->deckLinkInput->StartStreams() != S_OK) {
+        if (this->deckLinkInput->StartStreams() != S_OK)
+        {
             std::cout << "Failed to start video stream." << std::endl;
         }
     }
@@ -119,36 +126,36 @@ DeckLinkReceiver::~DeckLinkReceiver()
 HRESULT
 DeckLinkReceiver::VideoInputFrameArrived(IDeckLinkVideoInputFrame *videoFrame, IDeckLinkAudioInputPacket *)
 {
-    IDeckLinkVideoFrameAncillary* anc;
+    IDeckLinkVideoFrameAncillary *anc;
 
-    IDeckLinkVideoFrameAncillaryPackets* packets;
+    IDeckLinkVideoFrameAncillaryPackets *packets;
     IDeckLinkAncillaryPacket *packet;
 
-	//auto threadId = std::this_thread::get_id();
-   	//std::cout << "Frame threadId: " << threadId << std::endl;
+    //auto threadId = std::this_thread::get_id();
+    //std::cout << "Frame threadId: " << threadId << std::endl;
 
-    if (videoFrame->QueryInterface(IID_IDeckLinkVideoFrameAncillaryPackets, (void **)&packets) == S_OK) {
-        const uint8_t* data;
+    if (videoFrame->QueryInterface(IID_IDeckLinkVideoFrameAncillaryPackets, (void **)&packets) == S_OK)
+    {
+        const uint8_t *data;
         uint32_t size;
-    
+
         uint16_t vanc[MAX_WIDTH_VANC];
         size_t vanc_size = videoFrame->GetWidth();
 
-        // if (packets->GetFirstPacketByID('Q', 'R', &packet) == S_OK) {
-        //     if (packet->GetBytes(bmdAncillaryPacketFormatYCbCr10, (const void **)&data, &size) == S_OK) {
-        //         std::cout << "QR Len: " << size << " " << std::dec;
-        //         for (int i = 0; i < size; ++i) {
-        //             std::cout << std::hex << std::setfill('0') << std::setw(2) << data[i];
-        //         }
+        if (packets->GetFirstPacketByID('Q', 'R', &packet) == S_OK)
+        {
+            if (packet->GetBytes(bmdAncillaryPacketFormatYCbCr10, (const void **)&data, &size) == S_OK)
+            {
+                std::cout << "QS Len: " << size << " " << ToHex(data, size) << std::endl;
+            }
 
-        //         std::cout << std::endl;
-        //     }
+            packet->Release();
+        }
 
-        //     packet->Release();
-        // }
-
-        if (packets->GetFirstPacketByID('Q', 'S', &packet) == S_OK) {
-             if (packet->GetBytes(bmdAncillaryPacketFormatUInt8, (const void **)&data, &size) == S_OK) {
+        if (packets->GetFirstPacketByID('Q', 'S', &packet) == S_OK)
+        {
+            if (packet->GetBytes(bmdAncillaryPacketFormatUInt8, (const void **)&data, &size) == S_OK)
+            {
                 std::cout << "QS Len: " << size << " " << ToHex(data, size) << std::endl;
             }
             packet->Release();
@@ -165,13 +172,15 @@ DeckLinkReceiver::VideoInputFrameArrived(IDeckLinkVideoInputFrame *videoFrame, I
 HRESULT
 DeckLinkReceiver::VideoInputFormatChanged(BMDVideoInputFormatChangedEvents notificationEvents, IDeckLinkDisplayMode *newDisplayMode, BMDDetectedVideoInputFormatFlags detectedSignalFlags)
 {
-	auto threadId = std::this_thread::get_id();
-   	std::cout << "VideoInputFormatChanged threadId: " << threadId << std::endl;
+    auto threadId = std::this_thread::get_id();
+    std::cout << "VideoInputFormatChanged threadId: " << threadId << std::endl;
 
-    if (newDisplayMode) {
-        const char* name;
+    if (newDisplayMode)
+    {
+        const char *name;
 
-        if (newDisplayMode->GetName(&name) == S_OK) {
+        if (newDisplayMode->GetName(&name) == S_OK)
+        {
             std::cout << "Detected new video mode: " << name << std::endl;
         }
     }
@@ -185,18 +194,20 @@ DeckLinkReceiver::VideoInputFormatChanged(BMDVideoInputFormatChangedEvents notif
     return S_OK;
 }
 
-HRESULT 
-DeckLinkReceiver::QueryInterface(REFIID, void**) {
-    return S_FALSE;    
+HRESULT
+DeckLinkReceiver::QueryInterface(REFIID, void **)
+{
+    return S_FALSE;
 }
 
-ULONG 
-DeckLinkReceiver::AddRef() {
+ULONG
+DeckLinkReceiver::AddRef()
+{
     return 0;
 }
 
-ULONG 
-DeckLinkReceiver::Release() 
+ULONG
+DeckLinkReceiver::Release()
 {
     return 0;
 }
