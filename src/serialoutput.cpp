ded1e0961e5bf439a33a2213b20f7ec1d22fb4ea
lolsson <lolsson@lolssonsMBP1987.bme>
2020-03-05 22:40:10 +0100

    xD

--- src/serialoutput.cpp
+++ src/serialoutput.cpp
@@ -101,7 +101,7 @@ void SerialOutput::Write(uint8_t *data, size_t size)
     }
 
     uint8_t leadIn[] = {
-        1,             // The receiver is looking for 3 bytes of 254 in a row to start parsing
+        'h',             // The receiver is looking for 3 bytes of 254 in a row to start parsing
         1,             //
         1,             //
         (uint8_t)size,   // Size of packet, including header
