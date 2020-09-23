19317f1543bf2756271b9464b5154de451a3382e
lolsson <lolsson@lolssonsMBP2337.bme>
2020-09-23 14:42:47 +0200

    Updated standard baud rate

--- telemetry_receiver/uart.cpp
+++ telemetry_receiver/uart.cpp
@@ -13,7 +13,7 @@ InitUART()
     const int uart_buffer_size = (1024 * 2);
 
     uart_config_t uart_config = {
-        .baud_rate = 9600,
+        .baud_rate = 115200,
         .data_bits = UART_DATA_8_BITS,
         .parity    = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
